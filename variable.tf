79b6c5a2c2525f58e2a8ee912f8de8f2ab695d26
sairamkspj <sairamkspj@email.com>
2025-07-05 08:55:37 -0700

    vpc,internet gateway

---
+++ variable.tf
@@ -0,0 +1,28 @@
+variable "cvpc_idr" {
+   type = "string"
+
+}
+
+variable "enable_dns_hostnames" {
+   default = "true"
+
+}
+
+variable "common_tags" {
+    default = {}
+}
+
+variable "environment" {
+    type = "string"
+
+}
+
+variable "project_name" {
+    type = "string"
+
+}
+
+variable "vpc_tags" {
+    default = {}
+
+}
