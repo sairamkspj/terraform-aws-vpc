32be9e51d856b6ba334451d6b7e1819e69cd4434
sairamkspj <sairamkspj@email.com>
2025-07-05 23:58:36 -0700

    ne wchanges

--- local.tf
+++ local.tf
@@ -1,4 +1,4 @@
 locals {
     resource = "${var.project_name}-${var.environment}"
-    az_names= slice(data.aws_availability_zones.avaliable.names,0,2)
+    az_names= slice(data.aws_availability_zones.available.names,0,2)
 }
