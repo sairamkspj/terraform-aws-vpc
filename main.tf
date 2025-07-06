c2dff4b38f051b9313aea9b70c135fb30d2d8444
sairamkspj <sairamkspj@email.com>
2025-07-06 00:12:52 -0700

    ne wchanges

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "aws_subnet" "public" {
   count = length(var.public_subnet_cidrs)
   vpc_id = aws_vpc.main.id
   cidr_block = var.public_subnet_cidrs[count.index]
-  availability_zone = az_names[count.index]
+  availability_zone = local.az_names[count.index]
   tags = merge(
     var.common_tags,
     var.public_subnet_tags,
