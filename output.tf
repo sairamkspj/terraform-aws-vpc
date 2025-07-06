7d466eb13328c91737e9fddf55f84d295913b53a
sairamkspj <sairamkspj@email.com>
2025-07-05 21:18:02 -0700

    ne wchanges

--- output.tf
+++ output.tf
@@ -1,31 +1,3 @@
-# output  "vpc_id" {
-#   value       = aws_vpc.main.id
-# }
-
-# output "public_subnet_ids"{
-#   value = aws_subnet.public[*].id
-# }
-
-# output "private_subnet_ids"{
-#   value = aws_subnet.private[*].id
-# }
-
-# output "database_subnet_ids"{
-#   value = aws_subnet.database[*].id
-# }
-
-# output "database_subnet_group_name"{
-#   value = aws_db_subnet_group.default.name
-# }
-
-# output "az_info" {
-#     value = data.aws_availability_zones.available
-# }
-
-# output "default_vpc_info" {
-#   value = data.aws_vpc.default
-# }
-
-# output "main_route_table_info" {
-#   value = data.aws_route_table.main
-# }
+output "az_names"{
+  value = aws_availability_zones.avaliable
+}
